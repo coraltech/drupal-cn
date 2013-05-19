2150a8f1cc10259969bd01dab2b9798439d928e4
Derek Webb <derek@collectivecolors.com>
2013-05-19 03:34:43 -0400

    Added config.rb file to the document root.

---
+++ config.rb
@@ -0,0 +1,37 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# Paths to the base theme directories.
+css_dir = "sites/all/themes/mosaic/css"
+sass_dir = "sites/all/themes/mosaic/sass"
+javascripts_dir = "sites/all/themes/mosaic/js"
+project_type = :stand_alone
+
+
+# Images are located in the mosaic theme
+icons_dir  = "sites/all/themes/mosaic/assets/icons"
+mosaic_dir = "sites/all/themes/mosaic/assets/mosaic"
+vtiles_dir = "sites/all/themes/mosaic/assets/vtiles"
+htiles_dir = "sites/all/themes/mosaic/assets/htiles"
+
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+
+#preferred_syntax = :sass
+
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass scss sass
