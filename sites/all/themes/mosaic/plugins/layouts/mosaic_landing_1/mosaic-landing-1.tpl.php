2f161ae2a52cbcd15ce6e6740f7d2b5f844af991
Git account <git@cn.loc>
2013-06-13 00:42:53 +0000

    New region in mosaic_landing_1.

--- sites/all/themes/mosaic/plugins/layouts/mosaic_landing_1/mosaic-landing-1.tpl.php
+++ sites/all/themes/mosaic/plugins/layouts/mosaic_landing_1/mosaic-landing-1.tpl.php
@@ -1,5 +1,6 @@
 <div class="layout-landing-1">
-  <div class="inner-wrapper">
+  <div class='site-width'>
+    
     <?php if (!empty($content['leader'])): ?>
     <div class="container grid-24-last leader clear">
       <div class="limiter">
@@ -7,25 +8,24 @@
       </div>
     </div>
     <?php endif; ?>
-    
-    <div class='row'>
-      <?php if (!empty($content['main'])): ?>
-      <div class="container main-content grid-11">
-        <div class="limiter">
-          <?php print render($content['main']); ?>
-        </div>
+  
+  
+    <?php if (!empty($content['main'])): ?>
+    <div class="container main-content grid-11">
+      <div class="limiter">
+        <?php print render($content['main']); ?>
       </div>
-      <?php endif; ?>
-      
-      <?php if (!empty($content['secondary'])): ?>
-      <div class="container secondary-content grid-13-last">
-        <div class="limiter prepend-1">
-          <?php print render($content['secondary']); ?>
-        </div>
-      </div>
-      <?php endif; ?>
     </div>
+    <?php endif; ?>
     
+    <?php if (!empty($content['secondary'])): ?>
+    <div class="container secondary-content grid-13-last">
+      <div class="limiter prepend-1">
+        <?php print render($content['secondary']); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+  
     <?php if (!empty($content['follower'])): ?>
     <div class="container grid-24-last follower clear">
       <div class="limiter">
@@ -34,4 +34,12 @@
     </div>
     <?php endif; ?>
   </div>
+  
+  <?php if (!empty($content['sub_footer'])): ?>
+  <div class="container sub_footer clear">
+    <div class="limiter site-width">
+      <?php print render($content['sub_footer']); ?>
+    </div>
+  </div>
+  <?php endif; ?>
 </div>
