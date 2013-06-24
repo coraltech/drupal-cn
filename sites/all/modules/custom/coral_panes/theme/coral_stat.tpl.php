807692aa06bcc30d86ea17898ed73a1301374d2c
Git account <git@cn.loc>
2013-06-24 00:38:10 +0000

    Added theme tpls for node stats items and wrappers.

---
+++ sites/all/modules/custom/coral_panes/theme/coral_stat.tpl.php
@@ -0,0 +1,9 @@
+<?php
+  // Primary theme tpl for the individual stat
+  //  Override with coral_stat__[$stat_id].tpl.php
+?>
+<div class="coral-stat">
+  <span class='statid'><?php print $stat_title; ?></span>
+  <span class='statdata'><?php print $data; ?></span>
+</div>
+
---
+++ sites/all/modules/custom/coral_panes/theme/coral_stat_set.tpl.php
@@ -0,0 +1,6 @@
+<?php
+  // Primary wrapper template for each set of stats
+?>
+<div class="statset-wrap statset-<?php print $set_name; ?>">
+  <?php print $markup; ?>
+</div>
