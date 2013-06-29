aa3cca3ac139cca04ed7436e885b185bb016742c
Git account <git@cn.loc>
2013-06-29 11:16:54 +0000

    PORTAL-102 Added js file to read settings and initialize toc as it "Should have been"

--- sites/all/themes/mosaic/js/global/mosaicToc.js
+++ sites/all/themes/mosaic/js/global/mosaicToc.js
@@ -26,23 +26,67 @@ Drupal.mosaic = Drupal.mosaic || {};
     attach : function(context, settings) {
       try { // use try to ensure that if this breaks/fails, it won't break other stuff.
         if (Drupal.settings.mosaic.tocDefaults) {
-          
+          for (toc in Drupal.settings.mosaic.tocDefaults) {
+            new Drupal.mosaic.mosaicToc(toc, Drupal.settings.mosaic.tocDefaults); 
+          }
         }
       }
       catch (err) {
-        console.log('mosaicTocInit() reported errors... Any forms on the page? Error: '+err);
+        console.log('mosaicTocInit() reported errors... Perhaps you ordered tea? Error: '+err);
       }
     }
   };
 
   // Mosaic textfieldDefault takes one textfield and ensures it is
   //  set as it should be according to the specs in textfield_defaults.inc
-  Drupal.mosaic.tocInit = function(tocElement, tocDefaults) {
-    
-    
-    
+  Drupal.mosaic.mosaicToc = function(tocElement, tocDefaults) {
+    var mosaicToc = this;
+    // The tocElement refers to the ID that unifies the
+    //  header (the actual) with an anchor with the same href (+#)
+    var $header_element = $('#'+tocElement);
+    var $toc_anchor = $('.toc a[href="#'+tocElement+'"]');
+    this.$toc = $toc_anchor.parents('.toc');
+    this.$parent = $(this.$toc).parent();
+
+    var tocList = Backbone.View.extend({
+      
+      // Home
+      el: this.$parent,
+      toc: this.$toc,
+      
+      // Events ... null
+      
+      // Init
+      initialize: function() { 
+        mosaicToc.process(this.toc, tocElement, tocDefaults);
+        //_.bindAll(this, 'blurField', 'focusField'); 
+      },
+      
+      // Updates ... null
+    });
+        
     // Kick start!
     new tocList();
   };
+  
+  //---
+  
+  // Process each textfield
+  Drupal.mosaic.mosaicToc.prototype.process = function($toc, tocElement, tocDefaults) {
+    //console.log(tocElement);
+    //console.log(tocDefaults);
+    // establish unique ids for this .toc
+    var tocSeed = Math.floor(1000*Math.random());
+    $toc.attr('id', $toc.attr('id')+'_'+tocSeed);
+    
+    $tocItems = $toc.find('li a');
+    $tocItems.each(function(i) {
+      var id = $(this).attr('href');
+      $item = $(id);
+      var tagName = $item.prop('tagName');
+      $top = $item.next();
+      $top.removeClass('hide').addClass('toc-'+tagName).find('a').attr('href', '#'+$toc.attr('id'));
+    });    
+  }
 })(jQuery);
 
