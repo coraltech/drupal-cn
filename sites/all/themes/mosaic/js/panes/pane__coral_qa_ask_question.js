607e78c1cc4b403d8fec47ddff06752fc7917573
Git account <git@cn.loc>
2013-07-06 05:10:35 +0000

    First commit of pane__coral_qa_ask_question.js/

---
+++ sites/all/themes/mosaic/js/panes/pane__coral_qa_ask_question.js
@@ -0,0 +1,26 @@
+
+// This file ensures enter press support for the
+//  coral_tag_search custom pane.
+
+// Registers the Mosaic namespace
+Drupal.mosaic = Drupal.mosaic || {};
+
+// Document loaded!
+(function($) {
+  
+  Drupal.behaviors.mosaicQAInit = {
+    attach : function(context, settings) {
+      try {
+        // Any part of this that gets a mouse down we want
+        //  to focus the user on the textfield 
+        $('.pane-coral-qa-ask-question .field-name-body .text-full').attr('rows', '5'); // reset rows
+        $('.pane-coral-qa-ask-question .form-actions .form-submit').attr('value', Drupal.t('Submit question')); // reset submit text
+        $('.pane-coral-qa-ask-question .qa-msg .cont').height($('.pane-coral-qa-ask-question').height() - 32); // resize height of msg box
+      }
+      catch (err) {
+        console.log('mosaicQAInit() reported errors. Error: '+err);
+      }
+    }
+  };
+  
+})(jQuery);
