646b47328aa826925088f40c7bf95c7102d2542c
Git account <git@cn.loc>
2013-07-13 23:31:47 +0000

    Updating coral_ajax lib to enable node creation etc.

--- sites/all/modules/custom/coral_ajax/js/coral_ajax.js
+++ sites/all/modules/custom/coral_ajax/js/coral_ajax.js
@@ -2,6 +2,21 @@
 
   Drupal.coral_ajax = Drupal.coral_ajax || {};
   
+  Drupal.coral_ajax.create = function(type, entity, success, error) {
+    var data = {};
+    data[type] = entity;
+    
+    $.ajax({
+      url:'/system/'+type+'.json',
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      dataType: 'json',      
+      success: success,
+      error: error
+    });
+  };
+  
   Drupal.coral_ajax.get = function(type, id, success, error) {
     $.ajax({
       url:'/system/'+type+'/'+id+'.json',
@@ -11,29 +26,36 @@
     });
   };
   
-  Drupal.coral_ajax.put = function(type, id, entity, success, error) {
+  Drupal.coral_ajax.update = function(type, id, entity, success, error) {
+    var data = {};
+    data[type] = entity;
+    
     $.ajax({
-      url:'/system/'+type+'/'+id,
+      url:'/system/'+type+'/'+id+'.json',
       type: "PUT",
-      data: entity,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      dataType: 'json',      
+      success: success,
+      error: error
+    });
+  };
+  
+  Drupal.coral_ajax.remove = function(type, id, success, error) {
+    $.ajax({
+      url:'/system/'+type+'/'+id+'.json',
+      type: "DELETE",
+      dataType: 'json',
       success: success,
       error: error
     });
   };
   
   Drupal.coral_ajax.view_get = function(view_name, options, success, error) {
-    
-    var data = {};
-    
-    // Carry the passed in options over to the data object
-    for (op in options) {
-      if (options.hasOwnProperty(op)) data[op] = options[op];
-    }
-    
     $.ajax({
       url:'/api/view/'+view_name+'.json',
       dataType: 'json',
-      data: data,
+      data: options,
       success: success,
       error: error
     });
@@ -54,7 +76,15 @@
   };
   
   Drupal.coral_ajax.node_save = function(id, entity, success, error) {
-    Drupal.coral_ajax.put('node', id, entity, success, error);
+    Drupal.coral_ajax.update('node', id, entity, success, error);
+  };
+  
+  Drupal.coral_ajax.node_create = function(entity, success, error) {
+    Drupal.coral_ajax.create('node', entity, success, error);
+  };
+  
+  Drupal.coral_ajax.node_delete = function(id, entity, success, error) {
+    Drupal.coral_ajax.remove('node', id, entity, success, error);
   };
   
   Drupal.coral_ajax.user_load = function(id, success, error) {
