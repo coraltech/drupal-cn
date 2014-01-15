d205be5c867e8ea1f0f40f3d710730495ae3bc16
Git account <git@cn.loc>
2014-01-15 07:09:12 +0000

    PORTAL-461 Added active tag to the about primary link if the about submenu is present on page.

--- sites/all/themes/mosaic/js/panes/pane__block_menu_about.js
+++ sites/all/themes/mosaic/js/panes/pane__block_menu_about.js
@@ -11,12 +11,22 @@ Drupal.mosaic = Drupal.mosaic || {};
   Drupal.behaviors.mosaicAboutMenuInit = {    
     attach : function(context, settings) {
       try { // Use try to prevent systemic failure
+        
+        // Add some arrow containers
         var $about = $('.pane-block.pane-menu-about');
         if ($about.length > 0) {
           $about.find('.leaf a').each(function() {
             $(this).append('<span class="ar-out"><span class="ar-in"></span></span>');
           });
-        }    
+        }
+        
+        // Highligh about main menu link
+        var $main_items = $('.sf-menu.main li a');
+        $main_items.each(function() {
+          if ($(this).attr('href') == '/content/about-coral') {
+            $(this).addClass('active');
+          }
+        });
       }
       catch(err) {
         console.log('mosaicAboutMenuInit reported errors! Error: '+err);
