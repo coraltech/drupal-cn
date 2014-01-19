c6a4d9a25c9a72b751b878c7616abb100962c1d1
Git account <git@cn.loc>
2014-01-19 21:10:16 +0000

    PORTAL-479 Added mosaicMenu.js to turn off the click handler on the service links menu items.

---
+++ sites/all/themes/mosaic/js/global/mosaicMenu.js
@@ -0,0 +1,24 @@
+
+// JS file to manage some menu fixes or upgrades.-
+
+// Registers the Mosaic namespace
+Drupal.mosaic = Drupal.mosaic || {};
+
+// Document loaded!
+(function($) {
+  
+  // Initialize tinyMCE comps.
+  Drupal.behaviors.mosaicMenuInit = {
+    attach : function(context, settings) {
+      try { // use try to ensure that if this breaks/fails, it won't break other stuff.
+        // At the moment there is not much to do here...
+        var $user_menu = $('.sf-menu.service a').off('click');
+        
+      }
+      catch (err) {
+        console.log('mosaicMenuInit() reported errors. Error: '+err);
+      }
+    }
+  };
+  
+}) (jQuery);
