394be7ff86541b391f60b593b9a6b288f6fe8b59
Git account <git@cn.loc>
2014-02-17 07:03:47 +0000

    PORTAL-573 Using mosaicCore functionality to process view__documentation__related_view.js.

--- sites/all/themes/mosaic/js/views/view__documentation__related_view.js
+++ sites/all/themes/mosaic/js/views/view__documentation__related_view.js
@@ -10,27 +10,16 @@ Drupal.coralQA = Drupal.coralQA || {};
   Drupal.behaviors.coralRelatedDocsInit = {    
     attach : function(context, settings) {
       try { // Use try to prevent systemic failure
-        var getQueryParam = function(name) {
-          name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-          var regexS = "[\\?&]"+name+"=([^&#]*)";
-          var regex = new RegExp( regexS );
-          var results = regex.exec( window.location.href );
-          if( results == null )
-            return "";
-          else
-            return decodeURIComponent(results[1].replace(/\+/g, " "));
-        };
-        
+       	var params = Drupal.mosaic.core.getQueryParams(window.location.href);
         $relatedPane = $('.pane-documentation-related-view');
         if ($relatedPane.length) {
-          var tag = getQueryParam('tag');
-          if (tag == '') { // theres nothing "related" about it... no tags
+          if (Drupal.mosaic.core.objCount(params) == 0) { // theres nothing "related" about it... no tags
             $relatedPane.children('.pane-title').text('Documentation');
           }
         }
       }
       catch(err) {
-        console.log('coralNewQuestionsInit reported errors! Error: '+err);
+        console.log('coralRelatedDocsInit reported errors! Error: '+err);
       }
     }
   };  
