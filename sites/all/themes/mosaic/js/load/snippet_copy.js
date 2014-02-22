2a1d562e4442ebe94757061db036a346c34607a3
Git account <git@cn.loc>
2014-02-22 02:40:50 +0000

    PORTAL-593 Added styles for the Copy to clipboard link and added events and message display for same.

--- sites/all/themes/mosaic/js/load/snippet_copy.js
+++ sites/all/themes/mosaic/js/load/snippet_copy.js
@@ -44,10 +44,16 @@ Drupal.mosaic = Drupal.mosaic || {};
 				for (var resource in resources) {
 					// get target id 
 					var tgtID = core.createID($('#'+resource).find('.field-item:not(".ace_editor")'));
-					resources[resource].$resources.before('<button class="copy-btn" data-clipboard-target="'+tgtID+'">Copy to clipboard</button>');
-					var $btn = resources[resource].$resources.siblings('button.copy-btn'); 
-					var btnID = core.createID($btn); // add id to the new button
+					resources[resource].$resources.before('<div class="cp-cont"><span class="copy-btn" data-clipboard-target="'+tgtID+'">Copy to clipboard</span></div>');
+					var $btn = resources[resource].$resources.siblings('.cp-cont').find('.copy-btn');
+					core.createID($btn); // add id to the new button
 					Drupal.mosaic.core.objects.snippetNodes[node.id].resources[resource]['$copyBtn'] = $btn; // store the button
+
+					// copy message container
+					resources[resource].$resources.siblings('.cp-cont').append('<div class="copy-msg"><span class="arrow-up"></span><span class="msg"></span></div>');
+					var $cpMsg = resources[resource].$resources.siblings('.cp-cont').find('.copy-msg').hide();
+					core.createID($cpMsg); // add id to the cp-msg container
+					Drupal.mosaic.core.objects.snippetNodes[node.id].resources[resource]['$cpMsg'] = $cpMsg;
 				}
 
 				// Load the zeroclipboard js file
@@ -56,6 +62,20 @@ Drupal.mosaic = Drupal.mosaic || {};
 						for (var resource in resources) {
 							ZeroClipboard.config({ moviePath: "/sites/all/themes/mosaic/js/lib/ZeroClipboard/ZeroClipboard.swf" });
 							var client = new ZeroClipboard(resources[resource].$copyBtn); // start zeroclipboard
+
+							client.resource = resources[resource]; // attach a resource obj
+
+							client.on('mouseover', function(client, args) {
+								SC.updateMessage(client.resource, Drupal.t('Click to copy'));
+							});
+
+							client.on('mouseout', function(client, args) {
+								SC.updateMessage(client.resource, '', 250);
+							});
+
+							client.on('complete', function(client, args) {
+  							SC.updateMessage(client.resource, Drupal.t('Content copied'), 1500);
+							});
 						}
 					}
 				);
@@ -63,6 +83,32 @@ Drupal.mosaic = Drupal.mosaic || {};
 		}
 		catch (err) {
 			console.log('snippetCopy errored: '+err);
+		}
+	};
+
+
+	// Update the message
+	Drupal.mosaic.snippetCopy.prototype.updateMessage = function(resource, message, fadeTime) {
+		try {
+			// Clear the message from the container after a fadeOut
+			var clearMsg = function() {
+				if (fadeTime != undefined && Number(fadeTime)) {
+					resource.$cpMsg.fadeOut(fadeTime, function() {
+						resource.$cpMsg.find('.msg').text('');
+					});
+				}
+			};
+
+			if (message == '') { 
+				clearMsg(); // fade
+			}
+			else {
+				resource.$cpMsg.show().find('.msg').text(message); // set and show message 
+				clearMsg(); // start the fadeOut if there was one
+			}
+		}
+		catch (err) {
+			console.log('updateMessage errored: '+err);
 		}
 	};
 	
