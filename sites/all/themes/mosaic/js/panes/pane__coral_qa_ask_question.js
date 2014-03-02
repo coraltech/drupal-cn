50462fd0892f6173095cf7957ad85333ad9f5c2d
Git account <git@cn.loc>
2014-03-02 08:01:54 +0000

    PORTAL-632 Reducing height of qa_ask_question pane when user is not logged in.

--- sites/all/themes/mosaic/js/panes/pane__coral_qa_ask_question.js
+++ sites/all/themes/mosaic/js/panes/pane__coral_qa_ask_question.js
@@ -17,11 +17,12 @@ Drupal.mosaic = Drupal.mosaic || {};
         $('.pane-coral-qa-ask-question .node-question-form .form-actions .form-submit').attr('value', Drupal.t('Submit question')); // reset submit text
         
         var $form = $('.pane-coral-qa-ask-question form');
-        var formID = $form.attr('id');
+        var userLogin = $form.hasClass('user-login');
         var currentHeight = $form.height();
         
-        if (formID == 'user-login') {
-          if (currentHeight < 195) currentHeight = 195;
+        if (userLogin) {
+          if (currentHeight < 165) currentHeight = 165;
+          $form.height(currentHeight - $form.siblings('h3').outerHeight() - parseInt($form.css('padding-top'), 10) - parseInt($form.css('padding-bottom'), 10));
         }
         else {
           if (currentHeight < 235) currentHeight = 235;  
@@ -29,7 +30,7 @@ Drupal.mosaic = Drupal.mosaic || {};
         
         $('.pane-coral-qa-ask-question').height(currentHeight + 30);  
         $('.pane-coral-qa-ask-question .qa-msg .cont').height(currentHeight); // resize height of msg box
-        
+
         // add little icon holder - if it doesnt exist yet
         if (!$('.pane-coral-qa-ask-question .field-name-field-tags .icon').length) {
           $('.pane-coral-qa-ask-question .field-name-field-tags').prepend('<span class="icon"></span>');  
