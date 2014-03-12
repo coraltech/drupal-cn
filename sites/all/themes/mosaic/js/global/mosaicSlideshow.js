f034e2472d68a5d932fbefd7bbd9ed1bd603e851
Git account <git@cn.loc>
2014-03-12 19:24:49 +0000

    PORTAL-660 Commenting out the inclusion of mosaicSlideshow js as it is not needed atm/

--- sites/all/themes/mosaic/js/global/mosaicSlideshow.js
+++ sites/all/themes/mosaic/js/global/mosaicSlideshow.js
@@ -15,14 +15,14 @@ Drupal.mosaic = Drupal.mosaic || {};
       try { // Use try to prevent systemic failure
         // gather slideshows
         //console.log($('.views_slideshow_cycle_main').length); = 2
-        if ($('.views_slideshow_cycle_main').length > 0) {
-          $slideshows = $('.views_slideshow_cycle_main');
-          $slideshows.each(function(i) {
-            $container = $(this).parent();
+        //if ($('.views_slideshow_cycle_main').length > 0) {
+          //$slideshows = $('.views_slideshow_cycle_main');
+          //$slideshows.each(function(i) {
+            //$container = $(this).parent();
             //console.log($container);
-            new Drupal.mosaic.slideshow($(this), $container);
-          });
-        }
+            //new Drupal.mosaic.slideshow($(this), $container);
+          //});
+        //}
       }
       catch(err) {
         console.log('mosaicSlideshowInit reported errors! Error: '+err);
@@ -115,7 +115,7 @@ Drupal.mosaic = Drupal.mosaic || {};
   // Kickoff the slideshow
   Drupal.mosaic.slideshow.prototype.startup = function(container) {
     // We need to ensure the right size for all img containers
-    var $imgs = $(container).find('img');
+    var $imgs = $(container).find('img.slide-img');
     var maxW  = 0;
     $imgs.each(function() { if ($(this).width() > maxW) maxW = $(this).width(); });
     $imgs.each(function() { $(this).parent().width(maxW); });
