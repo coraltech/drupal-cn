03edee20f337ec0bf0819c0f4d7ff913371c9360
Git account <git@cn.loc>
2014-03-14 03:34:20 +0000

    PORTAL-682 Added responsive layout based on mosaic_landing_1

---
+++ sites/all/themes/mosaic/plugins/layouts/mosaic_landing_responsive/mosaic-landing-responsive.tpl.php
@@ -0,0 +1,46 @@
+<div class="layout-landing-responsive">
+  <div class='site-width clearfix'>
+    
+    <?php if (!empty($content['leader'])): ?>
+    <div class="container grid-24-last leader clear">
+      <div class="limiter">
+        <?php print render($content['leader']); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+  
+    <div class='row'>
+    <?php if (!empty($content['main'])): ?>
+    <div class="container main-content grid-13-last right">
+      <div class="limiter prepend-1">
+        <?php print render($content['main']); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+    
+    <?php if (!empty($content['secondary'])): ?>
+    <div class="container secondary-content grid-11 left">
+      <div class="limiter">
+        <?php print render($content['secondary']); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+    </div>
+  
+    <?php if (!empty($content['follower'])): ?>
+    <div class="container grid-24-last follower clear">
+      <div class="limiter">
+        <?php print render($content['follower']); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+  </div>
+  
+  <?php if (!empty($content['sub_footer'])): ?>
+  <div class="container sub_footer clear">
+    <div class="limiter site-width">
+      <?php print render($content['sub_footer']); ?>
+    </div>
+  </div>
+  <?php endif; ?>
+</div>
