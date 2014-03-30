44b16b1609d1797b35ba66c6bb32d5705fd2c9b8
Git account <git@cn.loc>
2014-03-30 03:14:51 +0000

    PORTAL-747 Updated code so that links are not reliant on superfish ids

--- sites/all/themes/mosaic/js/panes/pane__coral_community_tabs.js
+++ sites/all/themes/mosaic/js/panes/pane__coral_community_tabs.js
@@ -12,11 +12,7 @@ Drupal.mosaic = Drupal.mosaic || {};
     attach : function(context, settings) {
       try {
         // This just makes sure the right primary link is highlighted
-        $('#superfish-1 a[href="/content/community-documentation"]').addClass('active');
-        
-        // Need a place to drop this:
-        // It unbinds some stuipid gigya click handler from MY links!
-        $('#superfish-2 a').unbind('click');
+        $('#mini-panel-top_menus a[href="/content/community-documentation"]').addClass('active');
       }
       catch (err) {
         console.log('mosaicCommunityInit() reported errors. Error: '+err);
