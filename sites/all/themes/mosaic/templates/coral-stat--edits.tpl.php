fbb57d7934fa66d18878d8402d5a6666081ba49d
Git account <git@cn.loc>
2014-04-06 07:34:00 +0000

    PORTAL-783 Updated edits stat to not use semicolon, also added tpl and scss changes.

---
+++ sites/all/themes/mosaic/templates/coral-stat--edits.tpl.php
@@ -0,0 +1,9 @@
+<?php
+  // Edits stat tpl
+?>
+<div class="coral-stat <?php if ($class): ?>stat-<?php print $class; ?><?php endif; ?>">
+  <span class="icon"></span>
+  <span class='statdata'><?php print $data; ?></span>
+  <span class='statid'><?php print $stat_title; ?></span>
+</div>
+
