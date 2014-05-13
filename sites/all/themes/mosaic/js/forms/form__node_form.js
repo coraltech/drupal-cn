1ed235168b71c0f5e43c7e046e2dbed8d92a672b
Vagrant development account <admin@coralnexus.com>
2014-05-13 02:40:51 +0000

    PORTAL-907 PORTAL-906 Re-worked form node js to allow for multi filter tips to be in the same node form.

--- sites/all/themes/mosaic/js/forms/form__node_form.js
+++ sites/all/themes/mosaic/js/forms/form__node_form.js
@@ -13,114 +13,129 @@ Drupal.mosaic = Drupal.mosaic || {};
   Drupal.behaviors.mosaicNodeFormInit = {    
     attach : function(context, settings) { // play it again Sam...
       try { // Use try to prevent systemic failure
-      	// Disregard node-form instances that are right under
-      	// the body tag... this is a panels issue: it wraps
-      	// the page in a form. -_- 
-      	$nodeForm = $('.node-form').filter(function(index) {
-      		return !$(this).parent('body').length;
-      	});
-      	
-      	$nodeForm.each(function() {
-      		new Drupal.mosaic.nodeFormManager($(this));
-      	});    	
-			}
-			catch (err) {
-				console.log('mosaicNodeFormInit errored: '+err);
-			}
-		}
-	};
-
-	// @TODO see what happens when we have multiple filter tips in the same form.
-	// I expect it will not work at this time. It should iterate through them.
-	// As we only use one filter tips on any give node form, the issue is not a P1
-	Drupal.mosaic.nodeFormManager = function($form) {
-		try {
-			if (!$form.hasClass('nfproc')) {
-				this.id    = Drupal.mosaic.core.createID($form);
-				this.$form = $form.addClass('nfproc');
-				this.$filterLink  = this.$form.find('.filter-help a[href="/filter/tips"]').addClass(this.id+'-filter-tips');
-				this.$filterGuide = this.$form.find('.filter-guidelines').addClass('hidden').append('<a href="/filter/tips" class="more btn">Even more information</a>').hide();
-				this.events = {};
-				this.events['click .'+this.id+'-filter-tips'] = 'filterClick';
-
-				var nodeFormManager = this;
-      	var ManagerView = Backbone.View.extend({
-	        // Home
-	        el: this.$form,
-	        
-	        // Settings and conf
-	        nodeFormManager: nodeFormManager,
-	        
-	        // events
-	        events: nodeFormManager.events, // keyed events
-	                
-	        // Init
-	        initialize: function() {
-	          nodeFormManager.init();
-	          _.bindAll(this, 'filterClick');
-	        },
-	        
-	        filterClick: function(ev) {
-	          ev.preventDefault();
-	          nodeFormManager.handleFilterClick(ev);
-	        }
-      	});
-      	new ManagerView();
-			} // end if
-		}
-		catch (err) {
-			console.log('nodeFormManager errored: '+err);
-		}
-	};
+        // Update only on book form and loaded events
+        if (context.nodeName === '#document' || $(context).prop('id') == 'book-node-form') {
+        	// Disregard node-form instances that are right under
+          // the body tag... this is a panels issue: it wraps
+          // the page in a form. -_- 
+          $nodeForm = $('.node-form').filter(function(index) {
+            return !$(this).parent('body').length;
+          });
+          
+          $nodeForm.each(function() {
+            new Drupal.mosaic.nodeFormFilterManager($(this));
+          });
+        } 	
+      }
+      catch (err) {
+        console.log('mosaicNodeFormInit errored: '+err);
+      }
+    }
+  };
 
 
-	// Initilize the snippet node form
-	Drupal.mosaic.nodeFormManager.prototype.init = function() {
-		try {
-			// Move the field-body description above the filter formats fieldset
-			var $body  = this.$form.find('.field-name-body');
-			var $descr = $body.find('.description').detach();
-			$body.find('fieldset.filter-wrapper').before($descr);	
-		}
-		catch (err) {
-			console.log('Init errored: '+err);
-		}
+  // Manager of filter link clicks... how exciting, eh?
+	Drupal.mosaic.nodeFormFilterManager = function($form) {
+	  try {
+      this.events = {}; // storage for click events
+	    this.formID = Drupal.mosaic.core.createID($form);
+	    this.$form  = $form;
+	    this.$filterLinks = this.$form.find('.filter-help a[href="/filter/tips"]');
+
+	    // Filter links to those not processed yet
+	    this.$filterLinks = this.$filterLinks.filter(function(i) {
+	      return !$(this).hasClass('ffmproc');
+	    });
+
+	    // filter out links that have already been processed
+	    var nodeFormFilterManager = this; // also used in BB view
+	    this.$filterLinks.each(function(i) {
+	      $(this).addClass('ffmproc');
+	      var id = Drupal.mosaic.core.createID($(this));
+	      nodeFormFilterManager.events['click #'+id] = 'filterClick';
+	    });
+
+      // New items can be added to the form at any time so we have
+	    //  to be able to process the items repeatedly, safely.
+	    var FilterManagerView = Backbone.View.extend({
+        // Home
+        el: this.$form,
+
+        // Settings and conf
+        nodeFormFilterManager: nodeFormFilterManager,
+
+        // events
+        events: nodeFormFilterManager.events, // keyed events
+
+        // Init
+        initialize: function() {
+          nodeFormFilterManager.init();
+          _.bindAll(this, 'filterClick');
+        },
+
+        filterClick: function(ev) {
+          ev.preventDefault();
+          nodeFormFilterManager.handleFilterClick(ev);
+        }
+      });
+      new FilterManagerView();
+	  }
+	  catch (err) {
+	    console.log('nodeFormFilterManager errored: '+err);
+	  }
+
 	};
 
 
 	// Handle clicke event on filter tips link
-	Drupal.mosaic.nodeFormManager.prototype.handleFilterClick = function(ev) {
-		try {
-			ev.preventDefault();
-			if (this.$filterGuide.hasClass('hidden')) {
-				this.Overlay  = Drupal.mosaic.core.objects.mboverlay;
-	      this.$overlay = Drupal.mosaic.core.objects.mboverlay.$overlay;
-	      
-	      var NFMan = this;
-
-				this.$overlay.removeClass('hidden').fadeTo(250, .65, function() {
-					NFMan.Overlay.updateOverlay(false);
-					NFMan.initClose();
-				});
-
-        // .eq(0) because we only need to show one, if more than one exist
-				this.$filterGuide.eq(0).removeClass('hidden').show(); 
-			}
-		}
-		catch (err) {
-			console.log('handleFilterClick errored: '+err);
-		}
-	};
-	
-	
-	  // Set up close facilities
-  Drupal.mosaic.nodeFormManager.prototype.initClose = function() {
+  Drupal.mosaic.nodeFormFilterManager.prototype.handleFilterClick = function(ev) {
     try {
-      var NFMan = this;
+      ev.preventDefault();
       
-      this.$filterGuide.prepend('<div class="mbclose">x</div>');
-      this.$close = this.$filterGuide.children('.mbclose');
-      this.$close.css({
+      this.Overlay  = Drupal.mosaic.core.objects.mboverlay;
+      this.$overlay = Drupal.mosaic.core.objects.mboverlay.$overlay;
+      
+      var FFM = this;
+      this.$overlay.removeClass('hidden').fadeTo(250, .65, function() {
+        FFM.Overlay.updateOverlay(false);
+      });
+      
+      var $filter = $(ev.currentTarget);
+      var $guide  = $filter.parents('.filter-help').siblings('.filter-guidelines').show();
+      this.initClose($guide); // prepare to close it
+    }
+    catch (err) {
+      console.log('handleFilterClick errored: '+err);
+    }
+  };
+
+
+  // Initilize the snippet node form
+  Drupal.mosaic.nodeFormFilterManager.prototype.init = function() {
+    try {
+      // Move the field-body description above the filter formats fieldset
+      var $body  = this.$form.find('.field-name-body');
+      var $descr = $body.find('.description').detach();
+      $body.find('fieldset.filter-wrapper').before($descr);
+      
+      var FFM = this;
+      this.$filterLinks.each(function(i) {
+        $(this).parents('.filter-help').siblings('.filter-guidelines').hide();
+      })
+    }
+    catch (err) {
+      console.log('Init errored: '+err);
+    }
+  };
+
+
+  // Add closing capability
+  Drupal.mosaic.nodeFormFilterManager.prototype.initClose = function($guide) {
+    try {
+      var FFM = this;
+      
+      var $cls = $guide.prepend('<div class="mbclose">x</div>').children('.mbclose');
+      $cls.css({
         'cursor': 'pointer',
         'position': 'absolute',
         'z-index': 9,
@@ -128,25 +143,18 @@ Drupal.mosaic = Drupal.mosaic || {};
         'right': '-14px',
         'top': '-14px',
       });
-      this.$close.click(function() {
-        NFMan.closePane();
-      });
       
-      // Listen for changes in browser scroll position (100ms)
-      // Ripped from mosaicBox initClose()
-      this.closeInterval = setInterval(function() {
-        var docTop = $(window).scrollTop();
-        var qTop = NFMan.$filterGuide.offset().top;
-        var extra = 0;
-        var $navBar = $('#navbar-administration');
-        if ($navBar.length) extra = 40;
-        if ((docTop + extra) > qTop) {
-          NFMan.$close.css('top', docTop - qTop + extra);
-        }
-        else {
-          NFMan.$close.css('top', -14);
-        }
-      }, 100);
+      // Close click closes!
+      $cls.click(function() {
+        FFM.closeFilters($guide);
+        $(this).remove();
+      });
+
+      // Allow closing on overlay click
+      this.$overlay.off('click').click(function() {
+        FFM.closeFilters($guide);
+        $cls.remove();
+      });
     }
     catch (err) {
       console.log('initClose errored: '+err);
@@ -154,29 +162,23 @@ Drupal.mosaic = Drupal.mosaic || {};
   };
   
   
-    // Handle closing of the pane
-  Drupal.mosaic.nodeFormManager.prototype.closePane = function() {
+  // Handle closing of the pane
+  Drupal.mosaic.nodeFormFilterManager.prototype.closeFilters = function($guide) {
     try {
-      var NFMan = this;
+      var FFM = this;
       
       // hide the box
-      this.$filterGuide.addClass('hidden').slideUp(150);
+      $guide.addClass('hidden').slideUp(150);
       
       // update the overlay
       this.$overlay.fadeTo(250, 0, function() {
-        NFMan.$overlay.addClass('hidden');
-        NFMan.Overlay.updateOverlay(true);
+        FFM.$overlay.addClass('hidden');
+        FFM.Overlay.updateOverlay(true);
       });
-      
-      clearInterval(this.closeInterval); // clear close btn positioning timer
-      
-      this.$close.remove();
-      delete this.$close;
     }
     catch (err) {
-      console.log('closePane errored: '+err);
+      console.log('closeFilters errored: '+err);
     }
   };
-  
 })(jQuery);
 
