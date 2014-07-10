b3512824fd02faa22f7120efa94fbf44a593995b
Vagrant development account <admin@coralnexus.com>
2014-07-10 21:20:03 +0000

    PORTAL-1027 Added js to ensure wiki ask form is always viewable even in small screens.

--- sites/all/themes/mosaic/js/panes/pane__coral_wiki_ask_question.js
+++ sites/all/themes/mosaic/js/panes/pane__coral_wiki_ask_question.js
@@ -17,10 +17,18 @@ Drupal.mosaic = Drupal.mosaic || {};
         
         // @TODO: Set a unique identifier for each instance 
         // if there are more than one
-        
+        var WAM = {};
         if ($panel.length > 0) {
           $panel.each(function() {
-            new Drupal.mosaic.mosaicWAManager($(this));  
+             WAM = new Drupal.mosaic.mosaicWAManager($(this));  
+          });
+        }
+        
+        // Handle sizing
+        if (WAM.hasOwnProperty('$panel')) {
+          WAM.resizeWindow(); // init
+          $(window).resize(function() {
+            WAM.resizeWindow(); // resize events
           });
         }
       }
@@ -91,6 +99,7 @@ Drupal.mosaic = Drupal.mosaic || {};
     }
   };
 
+
   // Gather data
   Drupal.mosaic.mosaicWAManager.prototype.initializePane = function() {
     this.$form = this.$panel.find('.wiki-ask-question');
@@ -111,7 +120,10 @@ Drupal.mosaic = Drupal.mosaic || {};
       });
 
       // Show the form over the overlay      
-      this.$form.removeClass('hide').slideDown(250, function() {}).css(this.boxCss('show'));
+      this.$form.removeClass('hide').fadeIn(150, function() {}).css(this.boxCss('show'));
+      
+      // call last to ensure that the form is positioned as it needs to be.
+      this.resizeWindow();
     }
     catch (err) {
       console.log('handleClick errored: '+err);
@@ -125,7 +137,6 @@ Drupal.mosaic = Drupal.mosaic || {};
     try {
       if (state == 'show') {
         return {
-          'padding':'1em 1.5em',
           'position':'fixed',
           'z-index':'8',
           'background-color':'#fff'
@@ -133,7 +144,6 @@ Drupal.mosaic = Drupal.mosaic || {};
       }
       else {
         return {
-          'padding':'1em 1.5em',
           'position':'inherit',
           'background-color':'transparent'
         }
@@ -154,10 +164,23 @@ Drupal.mosaic = Drupal.mosaic || {};
         'cursor': 'pointer',
         'position': 'absolute',
         'z-index': 7,
-        'font-size': '2em',
-        'right': '-14px',
-        'top': '-14px',
+        'font-size': '2em'
       });
+
+      // compressed state happens in very small windows
+      if (this.$form.hasClass('compressed')) {
+        this.$close.css({
+          'right': '5px',
+          'top': '5px'
+        });
+      }
+      else {
+        this.$close.css({
+          'right': '-14px',
+          'top': '-14px'
+        });
+      }
+
       this.$close.click(function() {
         WAM.closePane();
       });
@@ -174,7 +197,9 @@ Drupal.mosaic = Drupal.mosaic || {};
           WAM.$close.css('top', docTop - qTop + extra);
         }
         else {
-          WAM.$close.css('top', -14);
+          if (!WAM.$form.hasClass('compressed')) {
+            WAM.$close.css('top', -14);
+          }
         }
       }, 100);
     }
@@ -190,12 +215,12 @@ Drupal.mosaic = Drupal.mosaic || {};
       var WAM = this;
       
       // hide the box
-      this.$form.slideUp(150, function() {
+      this.$form.fadeOut(150, function() {
         WAM.$form.css(WAM.boxCss('hide'));
       });
       
       // update the overlay
-      this.$overlay.fadeTo(250, 0, function() {
+      this.$overlay.fadeTo(75, 0, function() {
         WAM.$overlay.addClass('hidden');
         WAM.Overlay.updateOverlay(true);
       });
@@ -209,5 +234,36 @@ Drupal.mosaic = Drupal.mosaic || {};
       console.log('closePane errored: '+err);
     }
   };
+  
+  
+  // Resize the popup when needed
+  Drupal.mosaic.mosaicWAManager.prototype.resizeWindow = function() {
+    try {
+      var formTop    = Number(this.$form.position().top);
+      var formHeight = Number(this.$form.outerHeight());
+      var formHInner = this.$form.find('form').eq(0).outerHeight();
+      var winTop     = Number($('body').scrollTop());
+      var winHeight  = Number($(window).height());
+      var topSpace   = Number(this.$form.position().top) - Number($('body').scrollTop());
+      
+      if ((winHeight - 10) < formHInner) {
+        this.$form.css({
+          'top': '5px',
+          'height': (winHeight - 10) + 'px',
+          'overflow': 'auto'
+        }).addClass('compressed');
+        this.$form.find('.mbclose').css({'top': '5px', 'right': '5px'});
+      }
+      else {
+        this.$form.css({'height': '', 'overflow': ''}).removeClass('compressed');
+        this.$form.css('top', ((winHeight - formHeight) / 2)+'px');
+        this.$form.find('.mbclose').css({'top': '-14px', 'right': '-14px'});
+      }
+      
+    }
+    catch (err) {
+      console.log('resizeWindow errored: '+err);
+    }
+  }
   
 })(jQuery);
